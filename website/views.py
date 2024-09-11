baf0f0428501e847814dbdc9abae56ac7b7b4f29
MuliJob <munyoki912@gmail.com>
2024-09-11 20:31:18 +0300

    adding templates and running migration

--- website/views.py
+++ website/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return render(request, 'home.html')
+  context = {
+        'title': 'Welcome to My Django Site',
+        'message': 'This is the home page at the root URL.',
+    }
+  return render(request, 'digicraft.html', context)
