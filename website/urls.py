b2cb5e8e2f07fef438493f97625d15885e4ab70f
MuliJob <munyoki912@gmail.com>
2024-09-24 15:40:00 +0300

    added a new page to display services

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='digicraft'),
     path('pricing/', views.pricing, name = 'pricing'),
-    path('contactus/', views.contact, name = 'contact')
+    path('contactus/', views.contact, name = 'contact'),
+    path('services/', views.services, name= 'services')
 ]
