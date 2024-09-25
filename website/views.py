45927d97d56bbca442c916abe3581b748cbbaa92
MuliJob <munyoki912@gmail.com>
2024-09-25 10:23:46 +0300

    Added view function for sending newsletter email

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import NewsLetterRecipients
+from django.http import Http404, JsonResponse
 
 # Create your views here.
 def index(request):
@@ -15,3 +17,13 @@ def services(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def newsletter(request):
+    name = request.POST.get('your_name')
+    email = request.POST.get('email')
+
+    recipient = NewsLetterRecipients(name=name, email=email)
+    recipient.save()
+    send_welcome_email(name, email)
+    data = {'success': 'You have been successfully added to mailing list'}
+    return JsonResponse(data)
