fb67dbbfa3885d4e0acf68dc776ada1aaa0a4e37
MuliJob <munyoki912@gmail.com>
2024-09-25 13:31:40 +0300

    added animation and a toast when the subscribe button is pressed

--- website/static/js/newsletter.js
+++ website/static/js/newsletter.js
@@ -1,18 +1,65 @@
-$(document).ready(function(){
-  $('form').submit(function(event){
-    event.preventDefault()
-    form = $("form")
+$(document).ready(function() {
+  function getCookie(name) {
+      let cookieValue = null;
+      if (document.cookie && document.cookie !== '') {
+          const cookies = document.cookie.split(';');
+          for (let i = 0; i < cookies.length; i++) {
+              const cookie = cookies[i].trim();
+              if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                  break;
+              }
+          }
+      }
+      return cookieValue;
+  }
 
-    $.ajax({
-      'url':'/ajax/newsletter/',
-      'type':'POST',
-      'data':form.serialize(),
-      'dataType':'json',
-      'success': function(data){
-        alert(data['success'])
-      },
-    })// END of Ajax method
-    $("#id_email").val('')
-  }) // End of submit event
+  const csrftoken = getCookie('csrftoken');
 
-}) // End of document ready function
+  $('form').submit(function(event) {
+      event.preventDefault();
+      
+      const form = $(this);
+      const submitButton = $('#submit-button');
+
+      submitButton.html('<i class="fas fa-spinner fa-spin"></i> Subscribing...');
+      submitButton.prop('disabled', true);
+
+      $.ajax({
+          url: '/ajax/newsletter/',
+          type: 'POST',
+          data: form.serialize(),
+          headers: { 'X-CSRFToken': csrftoken },
+          dataType: 'json',
+          success: function(data) {
+              toastr.success(data['success'], 'Subscribed Successfully!');
+              form.trigger('reset');
+              submitButton.html('Subscribe');
+              submitButton.prop('disabled', false);
+          },
+          error: function(xhr, errmsg, err) {
+              toastr.error('There was an issue with your subscription.', 'Error');
+              submitButton.html('Subscribe');
+              submitButton.prop('disabled', false);
+          }
+      });
+  });
+});
+
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": true,
+  "positionClass": "toast-bottom-right",
+  "preventDuplicates": true,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+};
