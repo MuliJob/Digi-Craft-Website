337e39044655f933f7aeaa3decafc9c398b781fb
MuliJob <munyoki912@gmail.com>
2024-09-25 14:33:12 +0300

    added Message table to store send messages

--- website/forms.py
+++ website/forms.py
@@ -2,3 +2,9 @@ from django import forms
 
 class NewsLetterForm(forms.Form):
     email = forms.EmailField(label='Email')
+
+class MessageForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=30)
+    email = forms.EmailField(label='Email')
+    phone_number = forms.CharField(label='Phone number', max_length=10)
+    message = forms.CharField(label='Additional Message', max_length=1000)
--- website/models.py
+++ website/models.py
@@ -3,3 +3,12 @@ from django.db import models
 # Create your models here.
 class NewsLetterRecipients(models.Model):
     email = models.EmailField()
+
+class Message(models.Model):
+    name = models.CharField(max_length=30, blank=False)
+    email = models.EmailField(blank=False)
+    phone_number = models.CharField(max_length=10,blank=False)
+    message = models.CharField(max_length=1000, blank=False)
+
+    def __str__(self):
+        return self.name
