1bd0046cbd7932faa06a694b7bdedac14362687e
MuliJob <munyoki912@gmail.com>
2024-09-25 10:39:17 +0300

    added email function  to send email and email formats

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import NewsLetterRecipients
 from django.http import Http404, JsonResponse
+from .email import send_welcome_email
 
 # Create your views here.
 def index(request):
@@ -19,11 +20,10 @@ def about(request):
   return render(request, 'about.html')
 
 def newsletter(request):
-    name = request.POST.get('your_name')
     email = request.POST.get('email')
 
-    recipient = NewsLetterRecipients(name=name, email=email)
+    recipient = NewsLetterRecipients(email=email)
     recipient.save()
-    send_welcome_email(name, email)
+    send_welcome_email(email)
     data = {'success': 'You have been successfully added to mailing list'}
     return JsonResponse(data)
