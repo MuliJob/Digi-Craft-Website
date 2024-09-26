e086a7921c5ed51623828682aae3159365506dbf
MuliJob <munyoki912@gmail.com>
2024-09-26 15:59:36 +0300

    changed value to use environment variable

--- website/email.py
+++ website/email.py
@@ -1,9 +1,10 @@
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+import os
 
 def send_welcome_email(receiver):
     subject = 'Welcome to Digicraft, Elevate your online presence with us'
-    sender = 'jobmuli60@gmail.com'
+    sender = os.getenv('EMAIL_SENDER')
 
     text_content = render_to_string('email/digicraftemail.txt')
     html_content = render_to_string('email/digicraftemail.html',)
