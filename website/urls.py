c09d0a5f22f6ee64b7f1a06034580ed0ba4b857b
MuliJob <munyoki912@gmail.com>
2024-09-27 16:34:17 +0300

    added a new page to display more details of the services

--- website/urls.py
+++ website/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('contactus/', views.contact, name = 'contact'),
     path('services/', views.services, name= 'services'),
     path('about/', views.about, name = 'about'),
+    path('services/details', views.details, name = 'details'),
     path(r'newsletter/subscribe/', views.newsletter, name='newsletter'),
     path(r'contact/sendmessage/', views.message, name='message')
 ]
